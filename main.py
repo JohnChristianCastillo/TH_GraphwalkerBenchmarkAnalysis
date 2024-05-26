2d3aa2f60268d4a6da25b4d21844f77730cf5b18
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2024-05-26 22:21:09 +0200

    Use ReportFactory in main

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import argparse
 from pathlib import Path
 
+from report.report_factory import ReportFactory
 from src.models.benchmark import Benchmark
-from src.plotters.benchmark_plotter import BenchmarkPlotter
-from statistics.benchmark_statistics import BenchmarkStatistics
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -11,6 +10,9 @@ if __name__ == '__main__':
     parser.add_argument('--benchmark', '-b', type=str, help='Path to the benchmark directory.', required=True)
     parser.add_argument('--output', '-o', type=str, help='Path to the output directory. Default \".\"', default='.')
     parser.add_argument('--output_suffix', '-s', type=str, help='Suffix to append to the output directory name.')
+    parser.add_argument('--report_type', '-r', type=str,
+                        help='Type of report to generate. Default \"html\". Options: html, pdf, raw_data\nNote that pdf requires playwright to be installed. (\'playwright install\', using an activated python environment with playwright)',
+                        default='pdf')
     parser.add_argument('--whitelist', nargs='+', help='Whitelist of generators to include in the report.')
     parser.add_argument('--blacklist', nargs='+', help='Blacklist of generators to exclude from the report.')
     parser.add_argument('--verbose', '-v', action='store_true', help='Print verbose output.')
@@ -21,6 +23,9 @@ if __name__ == '__main__':
         print(f'Error: Path \"{input_path.absolute()}\" does not exist.')
         raise FileNotFoundError(input_path.absolute())
 
+    # Create early to fail fast if the report type is invalid
+    report_factory = ReportFactory(args.report_type)
+
     if args.verbose:
         print(f'Creating benchmark from \"{input_path}\"')
 
@@ -28,21 +33,11 @@ if __name__ == '__main__':
     if args.verbose:
         print(f'Loaded benchmark \"{benchmark}\" with {len(benchmark.report.generators)} generators.')
 
-    grouped_generators = benchmark.report.generators_grouped
-    if args.whitelist:
-        grouped_generators = {key: value for key, value in grouped_generators.items() if
-                              key.lower() in [x.lower() for x in args.whitelist]}
-
-    if args.blacklist:
-        grouped_generators = {key: value for key, value in grouped_generators.items() if
-                              key.lower() not in [x.lower() for x in args.blacklist]}
-
-    plots = BenchmarkPlotter.create_plots(grouped_generators, show=False)
-    statistics = BenchmarkStatistics.create_statistics(grouped_generators)
-
     output = Path(args.output)
     output = output / benchmark.name if not args.output_suffix else output / f'{benchmark.name}{args.output_suffix}'
     output.mkdir(parents=True, exist_ok=True)
 
     if args.verbose:
         print(f'Saving output to \"{output.absolute()}\"')
+
+    report_factory.create_report(benchmark, output, whitelist=args.whitelist, blacklist=args.blacklist)
